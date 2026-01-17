b8700215fe53d4b03f2f6d82406134bce0d0fffc
luthebao <luthebao1997@gmail.com>
2026-01-18 00:20:44 +0700

    feat: update application version to 1.0.6 and rename project to PolyXTools

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,10 +2,10 @@ package version
 
 // Version is the current application version
 // This should be updated before each release
-const Version = "1.0.5"
+const Version = "1.0.6"
 
 // GitHubOwner is the GitHub repository owner
 const GitHubOwner = "luthebao"
 
 // GitHubRepo is the GitHub repository name
-const GitHubRepo = "xtools"
+const GitHubRepo = "poly-xtools"
